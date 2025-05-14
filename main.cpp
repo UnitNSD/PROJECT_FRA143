a00a1066297f9325011c2692a92fb5bfd1bec5ff
stampprakan <stampprakan@gmail.com>
2025-05-14 17:04:55 +0700

    a

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "structure.h"
+#include <iostream>
+
+int main() {
+    for(int i;i < 10;i++){
+        int ammout_bullet = (rand()%8)+1;
+       std::cout<<ammout_bullet<<std::endl; 
+    }
+    
+}
